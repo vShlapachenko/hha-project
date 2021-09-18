e13e0119ebbb46337539e5295634a5b02fe9c7aa
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-09-18 15:06:13 -0700

    #2 Created / started Spring Boot project
    Added .gitignore to remove unnecessary jars / dependencies that will be generated by maven

---
+++ back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/HhaApplication.java
@@ -0,0 +1,13 @@
+package ca.sfu.cmpt373.pluto.fall2021.hha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhaApplication.class, args);
+    }
+
+}
