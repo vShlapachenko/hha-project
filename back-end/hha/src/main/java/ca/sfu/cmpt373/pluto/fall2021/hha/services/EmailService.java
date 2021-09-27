0410e1bcb57abc461d5336a395340c698fc72b31
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-09-26 19:46:15 -0700

    #22 Added email sent from dummy email address for loggin in to hha website

--- back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/services/EmailService.java
+++ back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/services/EmailService.java
@@ -8,27 +8,34 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.context.WebEngineContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
+    private final SpringTemplateEngine thymeleafTemplateEngine;
 
     @Value("${spring.mail.username}")
     private String emailFrom;
 
     public void invite(UserInvitation userInvitation) throws MessagingException {
 
-        var thymeleafContext = new Context();
-//        thymeleafContext.setVariables();
         var message = mailSender.createMimeMessage();
         var helper = new MimeMessageHelper(message, true, "UTF-8");
-        helper.setTo(userInvitation.email());
+        helper.setTo("valentyn.shlapachenko@gmail.com");
         helper.setFrom(emailFrom);
         helper.setSubject("Invitation To Join Hha");
+
+        var thymeleafContext = new Context();
+        thymeleafContext.setVariables(Map.of("recipientName", userInvitation.email()));
+        var htmlBody = thymeleafTemplateEngine.process("invite.html", thymeleafContext);
+
         helper.setText(htmlBody, true);
         mailSender.send(message);
 //        var message = new SimpleMailMessage();
