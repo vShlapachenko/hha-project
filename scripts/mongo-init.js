534ebf3f47df3480eaee189d4381f31d5605b87b
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-09-28 16:27:27 -0700

    #33 Adding mongo-init.js for initializing db

---
+++ scripts/mongo-init.js
@@ -0,0 +1,26 @@
+db = new Mongo().getDB("hha");
+
+db.createCollection('role', {capped: false});
+
+db.role.insert([
+    {"name": "ROLE_STAFF"},
+    {"name": "ROLE_ADMIN"},
+    {"name": "ROLE_HEAD_OF_DEP"},
+])
+
+roles = db.role.find().toArray()
+
+db.createCollection('hhaUser', {capped: false});
+
+//default password of default user is 1234
+db.hhaUser.insert([
+    {
+        email: "email@email.com", firstName: "firstName", lastName: "lastName",
+        password: "$2a$10$f/W5HK1BU/97RztYfZ531u/jxXb3GuPDlE6Qao/FOVSldXaXQSg3S",
+        activationLink: null, roles: [
+            new DBRef('role', roles[0]._id)
+        ],
+        deparment: null,
+        activationStatus: "ACTIVATED"
+    }
+])
