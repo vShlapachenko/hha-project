8937d66d08c667784e07e4e645425ae89f25d3ae
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-09-28 16:37:09 -0700

    #33 make default user and admin

--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -18,7 +18,7 @@ db.hhaUser.insert([
         email: "cmpt373.hha.project@gmail.com", firstName: "firstName", lastName: "lastName",
         password: "$2a$10$f/W5HK1BU/97RztYfZ531u/jxXb3GuPDlE6Qao/FOVSldXaXQSg3S",
         activationLink: null, roles: [
-            new DBRef('role', roles[0]._id)
+            new DBRef('role', roles[1]._id)
         ],
         deparment: null,
         activationStatus: "ACTIVATED"
