905ea120ee67e0f7aa351d91062141d981922476
Mayor2305 <jmayor@sfu.ca>
2021-10-09 21:01:17 -0700

    user profile without styling and change pass button not working

---
+++ front-end/hha-front-end/src/components/user_profile/userProfile.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import "./user_profile.css"
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+
+interface ProfileAttributes {
+    firstName: string,
+    lastName: string,
+    email: string,
+    staffNumber: string,
+    profileImage?: string,
+    department?: string
+  }
+  
+  const Profile = ({firstName, lastName, email, staffNumber, profileImage, department}: ProfileAttributes) => {
+    
+    return (
+        <Box sx={{width: 500, height: 800}}>   
+          {/* {profileImage && <Avatar alt={firstName} src={profileImage} sx={{ width: 100, height: 100 }} />} */}
+          
+          {/* <Chip label={department}/> */}
+        <h3 className="header" >User Profile</h3>
+        <TextField
+          className="text"
+          label="First Name"
+          defaultValue={firstName}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+
+        <TextField
+          className="text"
+          label="Last Name"
+          defaultValue={lastName} 
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+
+        <TextField
+          className="text"
+          label="Email"
+          defaultValue={email} 
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+
+        <TextField
+          className="text"
+          label="Staff Number"
+          defaultValue={staffNumber}
+          InputProps={{
+            readOnly: true
+          }}
+        />
+      <Box p={2} ml={18}>
+        <Button variant="contained">Change Password</Button>
+      </Box>
+      
+    </Box>
+    )
+  }
+  
+   
+  export default Profile;
