83d1c6ddd5b94ff9977e3b5465ee7d0b704c9320
jundi_chen <jundi_chen@sfu.ca>
2021-10-11 14:37:57 -0700

    added route support

--- front-end/hha-front-end/src/App.tsx
+++ front-end/hha-front-end/src/App.tsx
@@ -1,24 +1,41 @@
 import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
 import {Switch, Route, Redirect} from 'react-router-dom';
-import Login from './components/Login';
+import Login from './pages/loginPage/Login';
 import Navbar from './components/Navbar/Navbar';
 import {Context} from "./index";
 import {observer} from "mobx-react-lite"
+import UserService from "./service/UserService";
+import {User} from "./models/User";
 import Registration from './pages/registrationPage/Registration';
 
 
 function App() {
   const {store} = useContext(Context)
+  const [users, setUsers] = useState<User[]>([])
   useEffect(() => {
     if (localStorage.getItem('token')) {
       store.checkAuth()
     }
   }, [])
 
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data)
+    } catch (e) {
+
+    }
+  }
+
   if (!store.isAuthorized) {
     return (
-      <Login/>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/register" component={Registration} />
+        <Redirect from="*" to="/" />
+      </Switch>
     )
   } else 
   return (
@@ -28,7 +45,7 @@ function App() {
       <div>
         <button onClick={getUsers}> Get Users</button>
       </div>
-      {users.map(user => <div key={user.email}>THIS IS USER EMAIL {user.email}</div>)}
+      {users.map(user => <div key={user.email}>email:{user.email?user.email: "staff@hha.com"} firstName:{user.firstName} lastName: {user.lastName} </div>)}
     </div></>
   );
 }
