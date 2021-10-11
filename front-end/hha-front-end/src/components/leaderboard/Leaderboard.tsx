899e3fcef61973c8a06e310279de4ea097c80b00
ERIC <hka102@sfu.ca>
2021-10-10 20:56:59 -0700

    #5 leaderboard logic made

--- front-end/hha-front-end/src/components/leaderboard/Leaderboard.tsx
+++ front-end/hha-front-end/src/components/leaderboard/Leaderboard.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Leaderboard from 'react-leaderboard';
 
-this.state = {
-    data: [
-        {DepartmentName: "", Score: 0} //retrieve from backend
-    ],
+//using https://github.com/CGally/react-leaderboard library
+
+interface LeaderboardAttributes {
+    DepartmentName: string;
 }
 
-render(){
+const Leaderboard = ({DepartmentName}: LeaderboardAttributes) => {
+
     return (
-        <Leaderboard
-        data = {this.state.data}
-        sortBy = 'highScore'
-        labelBy = 'DepartmentName'/>
-    )
-}
+        <div>
+            <Leaderboard users={DepartmentName}></Leaderboard>
+        </div>
+    );
+
+};
+
+export default Leaderboard;
