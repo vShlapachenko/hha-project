922cd8d0e2d7e78dcd750b6573bfe45e909801a9
Mayor2305 <jmayor@sfu.ca>
2021-10-10 20:51:11 -0700

    merging master and resolving conflicts

--- front-end/hha-front-end/src/components/Login.tsx
+++ front-end/hha-front-end/src/components/Login.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Profile from './user_profile/userProfile'
 
 const Login = () => {
     return (
         <div>
-            <h1>Login page</h1>
+            {/* <h1>Login page</h1> */}
+            <Profile firstName={'nav'} lastName={'jot'} email={'laksfj@lfj.com'} staffNumber={'69'} />
         </div>
     );
 };
