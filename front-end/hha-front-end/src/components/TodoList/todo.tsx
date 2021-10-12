99478b03c968b40532b721f9bcf118fe46cc96d7
ERIC <hka102@sfu.ca>
2021-10-11 23:54:44 -0700

    finished design

---
+++ front-end/hha-front-end/src/components/TodoList/todo.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button'
+
+
+interface ToDoAttribute{
+    firstLine: string;
+    secondLine: string;
+    button: string;
+}
+
+const ToDo = ({firstLine, secondLine, button}:ToDoAttribute) => {
+
+    return(
+      <Grid container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box mx={2}
+        textAlign='center'
+          sx={{
+            width:3000,
+            height:150,
+            bgcolor: '#EEEEEE',
+            boxShadow: 1,
+            fontWeight: 'bold',
+            alignItems: 'center',
+            overflow: 'visible',
+            borderRadius: '12px'
+          }}>
+            <div>
+              <Typography
+              mt = {1}
+              color = '#000000'
+              fontWeight = 'bold'
+              variant = "h5"
+              >
+                {firstLine}
+              </Typography>
+            </div>
+            <div>
+              <Typography
+              mt = {2}
+              color = '#000000'
+              >
+                {secondLine}
+              </Typography>
+            </div>
+            <Box pt={2}>
+              <Button
+                onClick = {() => console.log('hi')}
+                type = {button}
+                variant="contained"
+                style = {{minWidth: '400px'}}
+                >
+                {button}
+              </Button>
+            </Box>
+        </Box>
+      </Grid>
+    );
+};
+export default ToDo;
