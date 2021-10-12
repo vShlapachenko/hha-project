3013e3d5250275d20a9bec10d89974b1ee02e26d
Skyler <xla276@sfu.ca>
2021-10-12 11:46:12 -0700

    resolve conflict

--- back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/repositories/HhaUserRepository.java
+++ back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/repositories/HhaUserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface HhaUserRepository extends MongoRepository<HhaUser, String> {
     HhaUser findByEmail(String email);
+
     HhaUser findByActivationLink(String activationLink);
+
     HhaUser findByConfirmationLink(String confirmationLink);
 }
