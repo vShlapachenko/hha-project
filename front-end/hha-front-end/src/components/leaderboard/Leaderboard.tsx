19624d045c0ef51c9ac58a32ad0574a2b8e83556
ERIC <hka102@sfu.ca>
2021-10-27 00:46:09 -0700

    edit on leaderboard css

--- front-end/hha-front-end/src/components/leaderboard/Leaderboard.tsx
+++ front-end/hha-front-end/src/components/leaderboard/Leaderboard.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography';
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid'
+// import Typography from '@mui/material/Typography';
+import './Leaderboard.module.css';
 
 interface LeaderboardAttribute {
     FirstDepartmentName: string;
@@ -14,149 +15,175 @@ interface LeaderboardAttribute {
 
 const Leaderboard = ({FirstDepartmentName, SecondDepartmentName, ThirdDepartmentName, FirstPoints, SecondPoints, ThirdPoints}:LeaderboardAttribute) => {
     return (
-      <Grid container
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="center"
-      >
-         <Box
-          sx={{
-            width:300,
-            height: 150,
-            bgcolor: '#009CC4',
-            boxShadow: 1,
-            fontWeight: 'bold',
-            alignItems: 'center',
-            overflow: 'hidden',
-            borderRadius: '12px'
-          }}> 
+      <div className='grid'>
+        <h1 className='grid'>hi</h1>
+      </div>
+//       <div className = 'grid'>
+//          <div className = 'box'>
           
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              2nd Place
-            </Typography>
-          </div>
+//           {/* <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               2nd Place
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              variant = "h4"
-              my = {1}
-              align = "center"  
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              {SecondDepartmentName}
-            </Typography>
-          </div>
+//           <div>
+//             <Typography
+//               variant = "h4"
+//               my = {1}
+//               align = "center"  
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {SecondDepartmentName}
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              {SecondPoints} points
-            </Typography>
-          </div>
-        </Box>
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {SecondPoints} points
+//             </Typography> */}
+//           {/* </div> */}
+//             <h1>hello</h1>
+//         </div>
 
-        <Box
-          sx={{
-            width:300,
-            height: 150,
-            bgcolor: '#823B8A',
-            boxShadow: 1,
-            fontWeight: 'bold',
-            alignItems: 'center',
-            overflow: 'hidden',
-            borderRadius: '12px'
-          }}> 
+//         {/* <div className = 'box'>
           
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              1st Place
-            </Typography>
-          </div>
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               2nd Place
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              variant = "h4"
-              my = {1}
-              align = "center"  
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              {FirstDepartmentName}
-            </Typography>
-          </div>
+//           <div>
+//             <Typography
+//               variant = "h4"
+//               my = {1}
+//               align = "center"  
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {SecondDepartmentName}
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-              color = "#FFFFFF"
-            >
-              {FirstPoints} points
-            </Typography>
-          </div>
-        </Box>
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {SecondPoints} points
+//             </Typography>
+//           </div>
 
-        <Box
-          sx={{
-            width:300,
-            height: 150,
-            bgcolor: '#EEEEEE',
-            boxShadow: 1,
-            fontWeight: 'bold',
-            alignItems: 'center',
-            overflow: 'hidden',
-            borderRadius: '12px'
-          }}> 
+//         </div> */}
+        
+        
+// {/* 
+//         <Box
+//           sx={{
+//             width:300,
+//             height: 150,
+//             bgcolor: '#823B8A',
+//             boxShadow: 1,
+//             overflow: 'hidden',
+//             borderRadius: '12px'
+//           }}> 
           
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-            >
-              3rd Place
-            </Typography>
-          </div>
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               1st Place
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              variant = "h4"
-              my = {1}
-              align = "center"  
-              fontWeight = "bold"
-            >
-              {ThirdDepartmentName}
-            </Typography>
-          </div>
+//           <div>
+//             <Typography
+//               variant = "h4"
+//               my = {1}
+//               align = "center"  
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {FirstDepartmentName}
+//             </Typography>
+//           </div>
 
-          <div>
-            <Typography
-              mt = {2}
-              align = "center"
-              fontWeight = "bold"
-            >
-              {ThirdPoints} points
-            </Typography>
-          </div>
-        </Box>
-        </Grid>
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//               color = "#FFFFFF"
+//             >
+//               {FirstPoints} points
+//             </Typography>
+//           </div>
+//         </Box>
+
+//         <Box
+//           sx={{
+//             width:300,
+//             height: 150,
+//             bgcolor: '#EEEEEE',
+//             boxShadow: 1,
+//             overflow: 'hidden',
+//             borderRadius: '12px'
+//           }}> 
+          
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//             >
+//               3rd Place
+//             </Typography>
+//           </div>
+
+//           <div>
+//             <Typography
+//               variant = "h4"
+//               my = {1}
+//               align = "center"  
+//               fontWeight = "bold"
+//             >
+//               {ThirdDepartmentName}
+//             </Typography>
+//           </div>
+
+//           <div>
+//             <Typography
+//               mt = {2}
+//               align = "center"
+//               fontWeight = "bold"
+//             >
+//               {ThirdPoints} points
+//             </Typography>
+//           </div>
+//         </Box> */}
+//         </div>
     );
 };
 
