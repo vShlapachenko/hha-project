62951d24662d2536efbc7d818995c422235f27b7
jundi_chen <jundi_chen@sfu.ca>
2021-11-05 17:56:35 -0700

    added in service

--- front-end/hha-front-end/src/service/CaseStudyService.ts
+++ front-end/hha-front-end/src/service/CaseStudyService.ts
@@ -6,6 +6,12 @@ export default class CaseStudyService {
 
     static getQuestions(caseName: any): Promise<AxiosResponse<CaseStudy>> {
         
-        return $api.post<CaseStudy>('/case-study/questions', {caseName});
+        return $api.post<CaseStudy>(`/case-study/questions?caseName=${caseName}`);
     }
+
+    static submitAnswers(id: any, submittedBy: any, caseName: string, submittedDate: any, entryList: Array<any>, photo: any): Promise<AxiosResponse<CaseStudy>> {
+        
+        return $api.post<CaseStudy>(`/case-study/submit`, {id, submittedBy, caseName, submittedDate, entryList, photo});
+    }
+
 }
