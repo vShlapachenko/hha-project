45523e78310a017f147bda4d204866050fac545c
jundi_chen <jundi_chen@sfu.ca>
2021-11-05 17:55:38 -0700

    render case study

--- front-end/hha-front-end/src/components/add_case_studies/AddCaseStudy.tsx
+++ front-end/hha-front-end/src/components/add_case_studies/AddCaseStudy.tsx
@@ -1,4 +1,5 @@
 import React, {FC, useContext, useState, useEffect} from "react";
+import { useHistory } from "react-router-dom";
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite"
 import Navbar from "../Navbar/Navbar";
@@ -7,17 +8,18 @@ import Dropdown from "../dropdown/Dropdown";
 import { render } from "@testing-library/react";
 import CaseStudyService from "../../service/CaseStudyService";
 import { CaseStudy } from "../../models/CaseStudy";
+import AddCaseStudyStyle from "./AddCaseStudy.module.css";
+import { Button } from "@mui/material";
+import Alert from '@mui/material/Alert';
 
 interface listName {
+    caseName: string
     questions: any
 }
 
-const AddCaseStudy = ({questions}: listName) => {
-
-    console.log("reached caseStudy");
-
-    console.log("questions are", questions);
-    
+const AddCaseStudy = ({caseName, questions}: listName) => {
+    const history = useHistory();
+    const [alert, setAlert] = useState(false);
     const splitString = (stringArray: string) => {
         let container: Array<string>;
         stringArray = stringArray?.replace(/[\{\}\[\]]/g, " ");
@@ -27,9 +29,6 @@ const AddCaseStudy = ({questions}: listName) => {
 
     let listOfQuestions = splitString(questions);
 
-    
-    
-
     const CASE_STUDY_OPTIONS = {
         PATIENT_STORY: "Patient Story",
         STAFF_RECOGNIZATION: "Staff Recognization",
@@ -37,147 +36,72 @@ const AddCaseStudy = ({questions}: listName) => {
         EQUIMENT_RECEIVED: "Equipment Received",
         OTHER_STORY: "Other Story"
     }
-    const [ patientName, setPatientName ] = useState("");
-  
-        // Similar to componentDidMount and componentDidUpdate:
-   
 
-    
+    let initialArray: any = [];
+    const [ answer, setAnswer ] = useState("");
+    const [ array, setArray ] = useState(initialArray);
     
 
     const setPatientNameFunc = (event: any) => {
+        setAlert(false);
         event.preventDefault();
-        setPatientName(event.target.value);
+        setAnswer(event.target.value);
+    }
+
+    const submitSave = () => {
+        setAlert(true);
+        const obj = {
+            question: "",
+            answer
+        }
+        setArray([...array, obj]);
+    }
+
+    async function submitAnswer() {
+        // submit
+        const body = {
+            "id": null,
+            "submittedBy": null,
+            "caseName": caseName,
+            "submittedDate": null,
+            "entryList": array,
+            "photo": null
+        }
+
+        try {
+            const response = await CaseStudyService.submitAnswers(body.id, body.submittedBy, body.caseName, body.submittedDate, body.entryList, body.photo);
+            console.log(response);
+            // history.push("/");
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const renderAlert =() => {
+        return <Alert severity="success">saved success!</Alert>;
+    }
+
+    const renderNothing =() => {
+        return <div></div>;
     }
 
     return (
         <div>
+             {alert?renderAlert(): renderNothing()}
            {listOfQuestions.map((item, key) => {
-               return <h5>{item}</h5>
+               return( 
+               <div>
+                    <h5>{item}</h5>
+                    {item?<Input key={item} userInput={answer} type="text" label="" onChangeFunc={setPatientNameFunc} />: <div></div>}
+                    <Button variant="outlined" onClick={submitSave} >save</Button>
+                  
+               </div>
+               )
            })} 
+           <Button variant="contained" onClick={submitAnswer} >Submit</Button>
+          
         </div>
     )
-        
-    
-    
-    
-    // <div>{listOfQuestions[0]}</div>
-            // hit endpoint casestudy/PATIENT_STORY
-
-
-        //     return (<div>
-        //        <div className="patientName">
-        //            <h5 >Enter the patient's name</h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-   
-        //        <div className="patientAge">
-        //            <h5 >Enter the patient's age </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >Patient's nationality </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >Why did the patient choose HCBH? </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >How long were they at HCBH? </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >What was their diagnosis? </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >Case study? </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //     </div>);
-        // } else if (name == CASE_STUDY_OPTIONS.STAFF_RECOGNIZATION) { 
-
-        //      // hit endpoint casestudy/STAFF_RECOGNIZATION
-             
-        //     return (<div>
-        //        <div className="patientName">
-        //            <h5 >Enter the staff name</h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-   
-        //        <div className="patientAge">
-        //            <h5 >Enter the staff job title </h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >what department does this staff memeber work in?</h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >How long have they been working in HCBH?</h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        <div className="patientName">
-        //            <h5 >Case study story</h5>
-        //            <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //        </div>
-        //        </div>);
-        // } else if (name == CASE_STUDY_OPTIONS.TRAINING_SESSION) {
-        //     return (<div>
-        //         <div className="patientName">
-        //             <h5 >Enter the patient'11111s name</h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-    
-        //         <div className="patientAge">
-        //             <h5 >Enter the patient's age </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //         <div className="patientName">
-        //             <h5 >Patient's nationality </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //          </div>
-        //      );
-        // } else if (name == CASE_STUDY_OPTIONS.EQUIMENT_RECEIVED) {
-        //     return (<div>
-        //         <div className="patientName">
-        //             <h5 >Enter the patient'11111s name</h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-    
-        //         <div className="patientAge">
-        //             <h5 >Enter the patient's age </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //         <div className="patientName">
-        //             <h5 >Patient's nationality </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //          </div>
-        //      );
-        // } else {
-        //     return (<div>
-        //         <div className="patientName">
-        //             <h5 >Enter the patient'11111s name</h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-    
-        //         <div className="patientAge">
-        //             <h5 >Enter the patient's age </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //         <div className="patientName">
-        //             <h5 >Patient's nationality </h5>
-        //             <Input userInput={patientName} type="text" label="" onChangeFunc={setPatientNameFunc} />
-        //         </div>
-        //          </div>
-        //      );
-        // }
-         
-    
-    
 }
 
 export default AddCaseStudy;
