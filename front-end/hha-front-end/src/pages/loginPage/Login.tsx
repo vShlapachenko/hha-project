8623f6032212a5b7cb7a511b6514bdd1aaf66881
nka57 <nka57@sfu.ca>
2021-11-08 10:52:02 -0800

    duplicate login.tsx deleted, login.css fixed

--- front-end/hha-front-end/src/pages/loginPage/Login.tsx
+++ front-end/hha-front-end/src/pages/loginPage/Login.tsx
@@ -3,6 +3,7 @@ import {Context} from "../../index";
 import {observer} from "mobx-react-lite"
 import "./Login.css";
 import logo_HHA from "./logo.svg"
+import {Button} from "@mui/material";
 
 
 const Login: FC = () => {
@@ -39,9 +40,12 @@ const Login: FC = () => {
             <a href="./forgotPassword">Forgot Password</a>
             <br />
             <br />
-            <button className= "loginButton" onClick={() => store.login(email, password)}>
+            <Button className= "loginButton"
+                    sx={{width: "475px", height: "42px", background: '#009CC4'}}
+                    variant="contained"
+                    onClick={() => store.login(email, password)}>
                 Login
-            </button>
+            </Button>
             <br />
             <br />
             <div className="plainText">If you are having any difficulties connecting to your account, please contact your <b>IT service</b> or <b>HHA representative</b> at <b>support@hha.com</b> </div>
