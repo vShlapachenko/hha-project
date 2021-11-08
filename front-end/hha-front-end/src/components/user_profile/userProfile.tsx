553c4a0cb2b1c445c000c81e8141f263ca1cd5ae
jundi_chen <jundi_chen@sfu.ca>
2021-11-08 13:20:34 -0800

    adjusted to cneter

--- front-end/hha-front-end/src/components/user_profile/userProfile.tsx
+++ front-end/hha-front-end/src/components/user_profile/userProfile.tsx
@@ -26,68 +26,72 @@ const Profile = ({firstName, lastName, email, staffNumber, profileImage, departm
     return (
         <div>
             <Navbar />
+            <div className="centerDiv">
             <h1 className="header-1">Profile</h1>
 
-            <div className="InfoPane">
+                <div className="InfoPane">
 
-                <h3 className="header-2">Personal Information</h3>
+                    <h3 className="header-2">Personal Information</h3>
 
-                <div className="first_last_name">
+                    <div className="first_last_name">
 
-                    <div className="fisrt_name">
+                        <div className="fisrt_name">
+                            <TextField
+                                sx={{width: "308px", height: "55px", background: '#FFFFFF', textSizeAdjust: "80%"}}
+                                label="First Name"
+                                defaultValue={firstName}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                        </div>
+
+                        <div className="Last_Name">
+                            <TextField
+                                sx={{width: "308px", height: "55px", background: '#FFFFFF'}}
+                                label="Last Name"
+                                defaultValue={lastName}
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="email_input">
                         <TextField
-                            sx={{width: "308px", height: "55px", background: '#FFFFFF', textSizeAdjust: "80%"}}
-                            label="First Name"
-                            defaultValue={firstName}
+                            sx={{width: "488px", height: "55px", background: '#FFFFFF'}}
+                            label="Email"
+                            defaultValue={email}
                             InputProps={{
                                 readOnly: true,
                             }}
                         />
                     </div>
 
-                    <div className="Last_Name">
+                    <div className="PSN_input">
                         <TextField
-                            sx={{width: "308px", height: "55px", background: '#FFFFFF'}}
-                            label="Last Name"
-                            defaultValue={lastName}
+                            sx={{width: "488px", height: "55px", background: '#FFFFFF'}}
+                            className="text"
+                            label="Staff Number"
+                            defaultValue={staffNumber}
                             InputProps={{
-                                readOnly: true,
+                                readOnly: true
                             }}
                         />
                     </div>
 
-                </div>
+                    <div className="change_pass_button">
+                        <Button onClick={redirectPage}
+                                sx={{width: "488px", height: "55px", background: '#009CC4'}}
+                                variant="contained">Change Password</Button>
+                    </div>
 
-                <div className="email_input">
-                    <TextField
-                        sx={{width: "488px", height: "55px", background: '#FFFFFF'}}
-                        label="Email"
-                        defaultValue={email}
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                    />
-                </div>
-
-                <div className="PSN_input">
-                    <TextField
-                        sx={{width: "488px", height: "55px", background: '#FFFFFF'}}
-                        className="text"
-                        label="Staff Number"
-                        defaultValue={staffNumber}
-                        InputProps={{
-                            readOnly: true
-                        }}
-                    />
-                </div>
-
-                <div className="change_pass_button">
-                    <Button onClick={redirectPage}
-                            sx={{width: "488px", height: "55px", background: '#009CC4'}}
-                            variant="contained">Change Password</Button>
                 </div>
 
             </div>
+           
 
         </div>
     )
