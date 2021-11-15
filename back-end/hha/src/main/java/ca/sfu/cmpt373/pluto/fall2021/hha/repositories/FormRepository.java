19739e703a072f5f200c14ec513d5e7930a1a4c3
abhijitk <abhijit_kaur@sfu.ca>
2021-11-15 15:22:56 -0800

    Forms find by label

--- back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/repositories/FormRepository.java
+++ back-end/hha/src/main/java/ca/sfu/cmpt373/pluto/fall2021/hha/repositories/FormRepository.java
@@ -4,4 +4,5 @@ import ca.sfu.cmpt373.pluto.fall2021.hha.models.Form;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FormRepository extends MongoRepository<Form, String> {
+    Form findByLabel(String label);
 }
