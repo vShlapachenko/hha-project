b75e80c2d3add0702894fc180679ad6a6246dfb0
jundi_chen <jundi_chen@sfu.ca>
2021-11-19 13:40:29 -0800

    added in helper function to get current user and change saveUser schema to include roles

--- front-end/hha-front-end/src/service/UserService.ts
+++ front-end/hha-front-end/src/service/UserService.ts
@@ -7,9 +7,14 @@ export default class UserService {
         return $api.get<User[]>('/hha-user/getUsers')
     }
 
-    static saveUser(firstName: String, lastName: String, password: String): Promise<AxiosResponse<User[]>> {
-        return $api.post<User[]>('/hha-user/saveUser', {firstName, lastName, password})
+    static saveUser(firstName: String, lastName: String, password: String, email: string, roles: Array<any>): Promise<AxiosResponse<User[]>> {
+        return $api.post<User[]>('/hha-user/saveUser', {firstName, lastName, password, email, roles})
     }
+
+    static getCurrentUser(email: any): Promise<AxiosResponse<User>> {
+        return $api.get<User>(`/hha-user/getCurrentUser?email=${email}`);
+    }
+
 }
 
 // i am following the following schema:
@@ -22,7 +27,7 @@ export default class UserService {
 //     private String password;
 //     private String activationLink;
 //     @DBRef(lazy = true)
-//     private Collection<Role> roles = new ArrayList<>();
+//     private List<Role> roles = new ArrayList<>();
 //     @DBRef
 //     private Department department;
 //     private ActivationStatus activationStatus;
