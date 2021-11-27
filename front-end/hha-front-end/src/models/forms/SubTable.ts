2e57bdfef06f637d7e4692113786adfa41b6dda0
den_ovsiienko <dovsiien@gmail.com>
2021-11-26 19:36:53 -0800

    Added export and exit buttons to form display

--- front-end/hha-front-end/src/models/forms/SubTable.ts
+++ front-end/hha-front-end/src/models/forms/SubTable.ts
@@ -1,6 +1,8 @@
 import { Column } from "./Column";
+import { SecondaryData } from './SecondaryData'
 
 export interface SubTable {
   label: string;
   columns: Column[];
+  secondaryData?: SecondaryData[]
 }
