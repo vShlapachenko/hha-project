f8a807eaec9c4ca7653c25841b9159c8ae0cb8e7
Valentyn Shlapachenko <vshlapac@sfu.ca>
2021-11-29 13:14:20 -0800

    #70 Todo list front end
    Refactored code to use single TOdo component. Adding hooks to make calls to backend

---
+++ front-end/hha-front-end/src/components/TodoList/Todo.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from "./Todo.module.css";
+import ToDoSingle from "./ToDoSingle";
+import {useHistory} from "react-router";
+
+function Todo(props : any) {
+  let history = useHistory();
+
+  const startForm = () => {history.push("/forms")}
+  const createStory = () => {history.push("/caseStudy")}
+
+  return (
+    <div>
+      <h1 className = {styles.textTODO}>
+        What's TODO List
+      </h1>
+
+      <div>
+        <ToDoSingle firstLine = {"Form for Sep, 2021 is ready to fill in."}
+                    secondLine = {"Click on the button to start filling in the form for Sep, 2021 or go to Forms Tab to start the form."}
+                    thirdLine = {"Start the form"}
+                    func = {startForm}/>
+      </div>
+
+      <div>
+        <ToDoSingle firstLine = {"Create New Story"}
+                    secondLine = {"Click on the button to create a new story or go to Case Study Tab"}
+                    thirdLine = {"Create New Story"}
+                    func = {createStory}/>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;
