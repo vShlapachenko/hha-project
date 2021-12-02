ec8a822ab2e14bea6a49f216bce3785d2b850b94
nka57 <nka57@sfu.ca>
2021-12-02 14:20:56 -0800

    add user css responsive done

--- front-end/hha-front-end/src/pages/addUserPage/AddUser.tsx
+++ front-end/hha-front-end/src/pages/addUserPage/AddUser.tsx
@@ -7,6 +7,7 @@ import styles from "./AddUser.module.css";
 import Input  from "../../components/Input/Input";
 import Dropdown from "../../components/dropdown/Dropdown";
 import { Button } from "@mui/material";
+import registrationStyle from "../registrationPage/Registration.module.css";
 
 const AddUser: FC = () => {
     const {store} = useContext(Context);
@@ -63,43 +64,46 @@ const AddUser: FC = () => {
         <div>
             <Navbar />
             <div className={styles.alignment}>
-            <div className={styles.header}>
-                <h5 >Create New Account</h5>
-            </div>
-            <div className={styles.enterEmail}>
-                <h5 >Enter the email of a new user</h5>
-            </div>
+                <div className={styles.header}>
+                    <h5 >Create New Account</h5>
+                </div>
+                <div className={styles.enterEmail}>
+                    <h5 >Enter the email of a new user</h5>
+                </div>
 
-            <div className= {styles.emailInput} >                  
-                <input value={email} type="text" placeholder="Email" onChange={setEmailFunc} />
-            </div>
+                <div className= {styles.emailInput} >
+                    <input className={styles.input}
+                           value={email} type="text" placeholder="Email" onChange={setEmailFunc} />
+                </div>
 
-            <div className={styles.confirmEmail}>
-                <h5 >Confirm email of a new user</h5>
-            </div>
-            <div className= {styles.confirmEmailInput} >                  
-                <input value={confirmEmail} type="text" placeholder="Confirm Email" onChange={setConfirmEmailFunc} />
-            </div>
+                <div className={styles.confirmEmail}>
+                    <h5 >Confirm email of a new user</h5>
+                </div>
+                <div className= {styles.confirmEmailInput} >
+                    <input className={styles.input}
+                           value={confirmEmail} type="text" placeholder="Confirm Email" onChange={setConfirmEmailFunc} />
+                </div>
 
-            <div className={styles.chooseTypeAccount}>
-                <h5 >Choose type of account</h5>
-            </div>
-            <div className= {styles.chooseTypeAccountDropdown}>
-                <Dropdown listItems={listItems} itemName={""} onChangeFunc={setDropdownFunc} initialValue={dropdown} />
-            </div>
-            <div className={styles.choosePassword}>
-                <h5 >Choose your password</h5>
-            </div>
-            <div className= {styles.password} >                  
-                <input value={password} type="text" placeholder="Password" onChange={setPasswordFunc} />
-            </div>
-            <div className={styles.submitButton} >
-                <Button sx={{ background: '#009CC4'}} variant="contained" onClick={createAccount} >Submit</Button>
-            </div>
+                <div className={styles.chooseTypeAccount}>
+                    <h5 >Choose type of account</h5>
+                </div>
+                <div className= {styles.chooseTypeAccountDropdown}>
+                    <Dropdown listItems={listItems} itemName={""} onChangeFunc={setDropdownFunc} initialValue={dropdown} />
+                </div>
+                <div className={styles.choosePassword}>
+                    <h5 >Choose your password</h5>
+                </div>
+                <div className= {styles.password} >
+                    <input className={styles.input}
+                           value={password} type="text" placeholder="Password" onChange={setPasswordFunc} />
+                </div>
+                <div className={styles.Button} >
+                    <Button sx={{ background: '#009CC4'}} variant="contained" onClick={createAccount} >Submit</Button>
+                </div>
 
-            <div className={styles.backToHomePage} >
-                <Button sx={{ background: '#009CC4'}} variant="contained" onClick={backToHomePage} >Back to Home Page</Button>
-            </div>
+                <div className={styles.Button} >
+                    <Button sx={{ background: '#009CC4'}} variant="contained" onClick={backToHomePage} >Back to Home Page</Button>
+                </div>
             </div>
         </div>
     );
