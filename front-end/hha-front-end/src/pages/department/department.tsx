bd80147213316b21ee99fe29178b3e0cdb509c76
nka57 <nka57@sfu.ca>
2021-12-06 01:03:24 -0800

    css fixed

--- front-end/hha-front-end/src/pages/department/department.tsx
+++ front-end/hha-front-end/src/pages/department/department.tsx
@@ -4,6 +4,7 @@ import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import Navbar from '../../components/Navbar/Navbar'
+import styles from "./department.module.css"
 import $api from '../../http'
 
 interface departmentAttributes {
@@ -40,17 +41,20 @@ const Department = ({department_name = 'Department Name'}: departmentAttributes)
     return (
         <div>
             <Navbar />
-            <h1> { department_name } </h1>
-
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                    <DesktopDatePicker
-                    label="Select Month and Year for the form"
-                    inputFormat="MM/yyyy"
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} />}
-                    />
-            </LocalizationProvider>     
+            <div className={styles.alignment}>
+                <h1 className={styles.header}> { department_name } </h1>
+                <div className={styles.date}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                            <DesktopDatePicker
+                            label="Select Month and Year for the form"
+                            inputFormat="MM/yyyy"
+                            value={value}
+                            onChange={handleChange}
+                            renderInput={(params) => <TextField {...params} />}
+                            />
+                    </LocalizationProvider>
+                </div>
+            </div>
             
         </div>
     );
